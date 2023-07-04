960fcc2f7e626950b52502228e384337b648b695
MartynasH <martynash@neurotechnology.com>
2023-07-04 11:00:44 +0300

    ADD: Card

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import "./App.css";
 import { Grid } from "@mui/material";
 import Header from "./components/Header/Header";
+import Card from "./components/Card/Card";
 
 function App() {
   return (
-    <Grid>
+    <Grid container xs={12}>
       <Header />
+      <Card />
+      <Card reverse={true} />
     </Grid>
   );
 }
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,8 +1,51 @@
 import { Box } from "@mui/system";
 import React from "react";
+import jetImage from "../../images/jet-airbase.jpg";
+import { Grid, Typography } from "@mui/material";
 
-const Card = () => {
-  return <Box>Card</Box>;
+export interface CardProps {
+  reverse?: boolean;
+}
+
+const Card: React.FC<CardProps> = ({ reverse = false }) => {
+  console.log(reverse);
+  return (
+    <Grid
+      container
+      direction={reverse ? "row" : "row-reverse"}
+      spacing={1}
+      justifyContent="space-around"
+      margin="5rem 0"
+    >
+      <Grid item container xs={6} alignContent="center">
+        <Typography variant="h2" width="100%" textAlign="end" margin="0 5rem">
+          Aircraft in a airbase
+        </Typography>
+        <Typography variant="h5" margin="5rem">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
+          cupiditate, suscipit velit distinctio amet quam magnam atque assumenda
+          ipsa. Impedit deserunt incidunt maxime quo hic consequuntur autem
+          laudantium. A, enim? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Omnis cupiditate, suscipit velit distinctio amet
+          quam magnam atque assumenda ipsa. Impedit deserunt incidunt maxime quo
+          hic consequuntur autem laudantium. A, enim? Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Omnis cupiditate, suscipit velit
+          distinctio amet quam magnam atque assumenda ipsa. Impedit deserunt
+          incidunt maxime quo hic consequuntur autem laudantium. A, enim? Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Omnis cupiditate,
+          suscipit velit distinctio amet quam magnam atque assumenda ipsa.
+          Impedit deserunt incidunt maxime quo hic consequuntur autem
+          laudantium. A, enim? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Omnis cupiditate, suscipit velit distinctio amet
+          quam magnam atque assumenda ipsa. Impedit deserunt incidunt maxime quo
+          hic consequuntur autem laudantium. A, enim?
+        </Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <img src={jetImage} alt="jet airbase" loading="lazy" width="100%" />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Card;
