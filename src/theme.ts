1463519ba0760d2cb2b2792883c7239224bb0216
MartynasH <martynash@neurotechnology.com>
2023-08-29 08:27:29 +0300

    FIX: WON

---
+++ src/theme.ts
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    squares: {
+      1: "blue",
+      2: "green",
+      3: "red",
+      4: "purple",
+      5: "maroon",
+      6: "turquoise",
+      7: "black",
+      8: "gray",
+    },
+    other: {
+      darkGrey: "rgb(189, 189, 189)",
+      lightGrey: "rgb(189, 189, 189)",
+    },
+  },
+};
